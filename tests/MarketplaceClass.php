9189f1cda694fd53d4da3a59945f1cf68508d7b9
Piotr Zalewa <piotr@zalewa.info>
2012-09-25 15:35:08 +0200

    get_urls tested

--- tests/MarketplaceClass.php
+++ tests/MarketplaceClass.php
@@ -25,7 +25,23 @@ class MarketplaceTest extends PHPUnit_Framework_TestCase
 		$s_secret = 'fake_secret';
 		$stab = $this->getMock('OAuth', array('fetch'), array($s_key, $s_secret));
 		$this->mockFetchUnauthorized($stab);
-		$oauth = new Marketplace($s_key, $s_secret);
-		$oauth->validate_manifest('http://example.com');
+		$marketplace = new Marketplace($s_key, $s_secret);
+		$marketplace->validate_manifest('http://example.com');
     }
+
+	public function testGetUrls()
+	{
+		$marketplace = new Marketplace('key', 'secret',
+			'domain', 'http', 80, 'prefix');
+		$r_urls = new ReflectionProperty('Marketplace', 'urls');
+		$r_urls->setAccessible(true);
+		$urls = $r_urls->getValue($marketplace);
+		$get_urls = new ReflectionMethod('Marketplace', 'get_url');
+		$get_urls->setAccessible(true);
+		foreach ($urls as $key => $path) {
+			$this->assertEquals(
+				$get_urls->invokeArgs($marketplace, array($key)),
+				'http://domain:80/prefix/api'.$urls[$key]);
+		}
+	}
 }
